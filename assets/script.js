47615eaada94c59daeee423fe2ff9fcce054a7b3
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-03-31 13:47:44 -0500

    forecast cards work

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-const API_KEY = "5592ab14c73d2e3c1705ad47c200a44e";
+var API_KEY = "5592ab14c73d2e3c1705ad47c200a44e";
 
-let geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=Austin&limit=1&appid="+API_KEY;
+var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=Austin&limit=1&appid="+API_KEY;
 
 var today = $("#current-conditions")
 var forecast = $("#forecast")
+var cards = $("#cards")
 var forecastBox = forecast[0];
 console.log(forecastBox)
 
@@ -27,10 +28,10 @@ fetch(geoCode)
         var icon = data.weather[0].icon
         var iconPic = "http://openweathermap.org/img/w/"+icon+".png"
         console.log(data)
-        console.log(dayjs.unix(data.dt).format('MM/DD/YYYY'))
+        console.log(dayjs.unix(data.dt).format('M/DD/YYYY'))
 
         var locDate = document.createElement("h2");
-        locDate.innerHTML = "Austin ("+dayjs().format('MM/DD/YYYY')+")";
+        locDate.innerHTML = "Austin ("+dayjs().format('M/DD/YYYY')+")";
         var iconEl = document.createElement("img");
         iconEl.setAttribute("src", iconPic)
         locDate.appendChild(iconEl);
@@ -56,10 +57,39 @@ fetch(geoCode)
     .then(function(data) {
         console.log(data.list);
         var forecast = [];
-        for (var i = 7; i < data.list.length; i += 8) {
+        for (var i = 6; i < data.list.length; i += 8) {
             forecast.push(data.list[i])
         }
         console.log(forecast);
+
+        var dayCard = document.createElement("div");
+        dayCard.setAttribute("class", "day-card");
+        var date = document.createElement("div");
+        date.setAttribute("class", "d-block");
+        date.innerHTML = dayjs.unix(forecast[0].dt).format('M/DD/YYYY')
+        
+        dayCard.appendChild(date);
+
+        var icon = forecast[0].weather[0].icon;
+        var iconPic = "http://openweathermap.org/img/w/"+icon+".png";
+        
+        var iconEl = document.createElement('img');
+        iconEl.setAttribute('src', iconPic);
+        dayCard.appendChild(iconEl);
+
+        var temp = document.createElement("p");
+        temp.innerHTML = "Temp: " + forecast[0].main.temp + "°F";
+        dayCard.appendChild(temp);
+
+        var wind = document.createElement("p");
+        wind.innerHTML = "Wind: " + forecast[0].wind.speed + " MPH";
+        dayCard.appendChild(wind);
+
+        var humidity = document.createElement("p");
+        humidity.innerHTML = "Humidity: " + forecast[0].main.humidity + "%";
+        dayCard.appendChild(humidity)
+        
+        cards[0].appendChild(dayCard);
 
 
     })
